d6fdc448aa32b28fad46e3d45fe6a23193261561
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 20:03:03 +0900

    add : mのスケールを調整

--- conditions.py
+++ conditions.py
@@ -9,7 +9,7 @@ T_ORDER = np.power(10, 13)
 DT = 0.001  # * 10^13 s
 T_END = 2
 
-M_cc = 1 * M_SUN  # cloud core
+M_cc = 1  # cloud core
 G = 6.67259 / np.power(10, 8)
 Rho_init = 10 ** -17  # gcm-3 from 10-17to 10-20
 R_cc = 10000 * AU  # AU 1000 to 30000
