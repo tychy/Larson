4e4327a66eadb9748b71cf2bdabb8d5d4c590eb0
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 16:17:56 +0900

    add : vstackをラップ

--- test_modules.py
+++ test_modules.py
@@ -1,1 +1,19 @@
-from utils import CFL
+from conditions import R_cc, GRID
+from utils import CFL, vstack_n
+import numpy as np
+import unittest
+
+
+class TestModules(unittest.TestCase):
+    def test_vstack_n(self):
+        r = vstack_n(np.arange(0, R_cc + GRID, R_cc / GRID), 3)
+        self.assertTrue(r.shape[0] == 3)
+
+    def test_CFL(self):
+        r = vstack_n(np.arange(0, R_cc + GRID, R_cc / GRID), 3)
+        dt = CFL(r[r.shape[0] - 1])
+        self.assertTrue(dt == 0.01)
+
+
+if __name__ == "__main__":
+    unittest.main()
