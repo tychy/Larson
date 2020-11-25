3c1a9e2c6b280873fc311d538e48a78d73108b41
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 20:21:11 +0900

    add :オーダーを調整

--- conditions.py
+++ conditions.py
@@ -11,5 +11,5 @@ T_END = 2
 
 M_cc = 1  # cloud core
 G = 6.67259 / np.power(10, 8)
-Rho_init = 10 ** -17  # gcm-3 from 10-17to 10-20
-R_cc = 10000 * AU  # AU 1000 to 30000
+Rho_init = M_SUN / np.power(10, 17)  # gcm-3 from 10-17to 10-20
+R_cc = 10000  # AU 1000 to 30000
