6590cbe87b5661bd426421863ec1011a83ffae18
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 19:05:07 +0900

    add : tのオーダーを改善

--- conditions.py
+++ conditions.py
@@ -5,9 +5,9 @@ R_SUN = 7 * np.power(10, 10)
 TMP_init = 10  # or 5 or 30
 AU = 1.4598 * np.power(10, 13)  # cm
 GRID = 200
-DT = 0.001  # * 10^13 s
 T_ORDER = np.power(10, 13)
-T_END = 2 * T_ORDER
+DT = 0.001  # * 10^13 s
+T_END = 2
 
 M_cc = 1 * M_SUN  # cloud core
 G = 6.67259 / np.power(10, 8)
