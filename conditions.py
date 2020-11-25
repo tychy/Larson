7ade66459363fb8916b3d70489f5d57278e7663a
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 23:13:20 +0900

    update : 定数を変更

--- conditions.py
+++ conditions.py
@@ -7,12 +7,11 @@ AU = 1.4598 * np.power(10, 13)  # cm
 GRID = 200
 T_ORDER = np.power(10, 13)
 DT = 0.001  # * 10^13 s
-T_END = 2
+T_END = 1.5
 
 M_cc = 1  # cloud core
 
 G = 6.67259 / np.power(
-    10, 8 + 3 * np.log10(AU) + np.log10(M_SUN) - 2 * np.log10(T_ORDER)
-)
-Rho_init = M_SUN * M_cc / np.power(10, 17)  # gcm-3 from 10-17to 10-20
+    10, 8
+)  # + 3 * np.log10(AU) + np.log10(M_SUN) - 2 * np.log10(T_ORDER))
 R_cc = 10000  # AU 1000 to 30000
