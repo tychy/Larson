c045f4c333c34f304df64a3ac1b5cb09ffd61caa
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 18:21:32 +0900

    add : csのテスト

--- test_modules.py
+++ test_modules.py
@@ -1,5 +1,5 @@
-from conditions import R_cc, GRID
-from utils import CFL, vstack_n
+from conditions import R_cc, GRID, T_ORDER
+from utils import CFL, vstack_n, get_cs
 import numpy as np
 import unittest
 
@@ -13,6 +13,12 @@ class TestModules(unittest.TestCase):
         r = vstack_n(np.arange(0, R_cc + GRID, R_cc / GRID), 3)
         dt = CFL(r[r.shape[0] - 1])
         self.assertTrue(dt == 0.01)
+
+    def test_get_cs(self):
+        cs = get_cs(10)
+        self.assertTrue(cs == 30000)
+        cs = get_cs(1000)
+        self.assertTrue(cs == 300000)
 
 
 if __name__ == "__main__":
