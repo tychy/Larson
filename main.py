f2ed01b7019beb322a0694b3ceb52317ab14e2e1
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 19:05:25 +0900

    fix : vstackの扱いを修正

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from conditions import M_cc, G, Rho_init, R_cc
 from conditions import DT, TMP_init, AU, GRID, T_END
-from utils import CFL, vstack_n
+from utils import CFL, vstack_n, get_cs
 
 
 def next(idx, t_h, deltat, v, r, rho, p, tmp, m, deltam, r_h, r_l, p_l, Q):
@@ -25,10 +25,10 @@ def next(idx, t_h, deltat, v, r, rho, p, tmp, m, deltam, r_h, r_l, p_l, Q):
         )
     v_res[0] = 0
     v_res[v_res.shape[0] - 1] = 0
-    v.vstack(v_res)
+    v = np.vstack((v, v_res))
 
     r_res = r[idx] + v_res * t_h[idx]
-    r.vstack(r_res)
+    r = np.vstack((r, r_res))
     rho_res = np.zeros(r_res.shape[0] - 1)
     for i in range(rho_res.shape[0]):
         rho_res[i] = (
@@ -36,34 +36,46 @@ def next(idx, t_h, deltat, v, r, rho, p, tmp, m, deltam, r_h, r_l, p_l, Q):
             * deltam[idx][i]
             / (np.power(r_res[idx][i + 1], 3) - np.power(r_res[idx][i], 3))
         )
-    rho.vstack(rho_res)
-    return
+    rho = np.vstack((rho, rho_res))
+    return v, r, rho, p, tmp
+
+
+def calc_t(idx, r, t, t_h, deltat, tmp):
+    t_diff = CFL(r[idx], tmp.mean(), 0.5)
+    t = np.append(t, t[idx] + t_diff)
+    t_h = np.append(t_h, t_diff)
+    deltat = np.append(deltat, (t_diff + t_h[idx - 1]) / 2)
+    if True:
+        print("t_diff:", t_diff)
+        print("t:", t)
+        print("t_h:", t_h)
+        print("deltat:", deltat)
+    return t, t_h, deltat
 
 
 def calc_lambda(idx, v, r, p, t_h, r_l, p_l):
-    r_res = r[idx] + (ht[idx] - t_h[idx - 1]) * v[idx] / 4
-    p_res = p[idx] + (ht[idx + 1] - t_h[idx - 1])(p[idx] - p[idx - 1]) / (
-        4 * t_h[idx - 1]
-    )
-    r_l.vstack(r_res)
-    p_l.vstack(p_res)
-    return
+    t_diff = t_h[idx] - t_h[idx - 1]
+    r_res = r[idx] + t_diff * v[idx - 1] / 4
+    p_res = p[idx] + t_diff * (p[idx] - p[idx - 1]) / (4 * t_h[idx - 1])
+    r_l = np.vstack((r_l, r_res))
+    p_l = np.vstack((p_l, p_res))
+    return r_l, p_l
 
 
 def calc_deltam(m, deltam):
     m_res = np.zeros(m.shape[1] - 1)
     for i in range(m_res.shape[0]):
         m_res[i] = m[i + 1] - m[i]
-    deltam.vstack(m_res)
-    return
+    deltam = np.vstack((deltam, m_res))
+    return deltam
 
 
 def calc_half(r, r_h):
     r_res = np.zeros(r.shape[1] - 1)
     for i in range(r_res[0]):
         r_res[i] = np.power(np.power(r[i], 3) + np.power(r[i], 3), 1 / 3)
-    r_h.vstack(r_res)
-    return
+    r_h = np.vstack((r_h, r_res))
+    return r_h
 
 
 def calc_Q(idx, l_const, t_h, v, r, rho, Q):
@@ -74,24 +86,23 @@ def calc_Q(idx, l_const, t_h, v, r, rho, Q):
             r[idx - 1][i + 1] - r[idx - 1][i]
         )
     Q_res = -2 * mu * Q_res
-    Q.vstack(Q_res)
-    return
+    Q = np.vstack((Q, Q_res))
+    return Q
 
 
 def main():
     # v_i+\half = idx[i]
     # 初期化
-    t = np.arange(0, T_END, DT)
-
-    t_h = np.zeros(t.shape - 1)
-    for idx in range(t.shape - 1):
+    t = np.array([0, 0.000002, 0.000002])
+    t_h = np.zeros(t.shape[0] - 1)
+    for idx in range(t.shape[0] - 1):
         t_h[idx] = t[idx + 1] - t[idx]
-    deltat = np.zeros(t.shape)
-    for idx in range(t.shape - 1):
+    deltat = np.zeros(t.shape[0])
+    for idx in range(t_h.shape[0] - 1):
         deltat[idx + 1] = (t_h[idx + 1] + t_h[idx]) / 2
-    m = vstack_n((np.arange(0, M_cc + GRID, M_cc / GRID),3)
+    m = vstack_n(np.arange(0, M_cc + GRID, M_cc / GRID), 3)
     v = np.zeros([2, GRID + 1])
-    r = vstack_n((np.arange(0, R_cc + GRID, R_cc / GRID),3)
+    r = vstack_n(np.arange(0, R_cc + GRID, R_cc / GRID), 3)
     p = np.zeros([3, GRID])
     rho = np.ones([3, GRID]) * 1.0 / GRID
     tmp = np.ones([3, GRID]) * 10
@@ -105,17 +116,22 @@ def main():
     deltam = np.zeros([3, GRID])
 
     # main loop
-    counter = 0
-    while t < T_END:
-        print("counter:",counter)
+    counter = 2
+    cur_t = 0.0
+    while cur_t < T_END:
+        print("counter:", counter)
         # debug
-        if counter == 2:
+        if counter == 4:
             break
-        calc_lambda(counter, v, r, p, t_h, r_l, p_l)
-        calc_deltam(m, deltam)
-        calc_half(r, r_h)
-        calc_Q(counter, l_const, t_h, v, r, rho, Q)
-        next(counter, t_h, deltat, v, r, rho, p, tmp, m, deltam, r_h, r_l, p_l, Q)
+        t, t_h, deltat = calc_t(counter, r, t, t_h, deltat, tmp)
+        r_l, p_l = calc_lambda(counter, v, r, p, t_h, r_l, p_l)
+        deltam = calc_deltam(m, deltam)
+        r_h = calc_half(r, r_h)
+        Q = calc_Q(counter, l_const, t_h, v, r, rho, Q)
+        v, r, rho, p, tmp = next(
+            counter, t_h, deltat, v, r, rho, p, tmp, m, deltam, r_h, r_l, p_l, Q
+        )
+        print(v)
         counter += 1
 
 
