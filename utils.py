8a448c46e5385c183b11ab6925a853b2cc4f08cd
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-11-25 18:21:59 +0900

    add : get_cs

--- utils.py
+++ utils.py
@@ -1,7 +1,18 @@
 import numpy as np
 
+from conditions import T_ORDER
 
-def CFL(x):
+
+def CFL(x, tmp=10.0, param=1.0):
+    assert x.ndim == 1, "x must be one dimentional array."
+    assert x.shape[0] > 1, "x must be array."
+    assert param > 0, "param must be positive."
+    dx_min = x[1] - x[0]
+    for i in range(x.shape[0] - 1):
+        dx_min = min(dx_min, x[i + 1] - x[i])
+    dt = dx_min / get_cs(tmp)
+    print("dt:", dt)
+    print("dt(/10^13s=10^6y):", dt / T_ORDER)
     return 0.01
 
 
@@ -10,3 +21,10 @@ def vstack_n(v, n):
     for _ in range(n - 1):
         x = np.vstack([x, v])
     return x
+
+
+def get_cs(tmp):
+    # 10K 0.3km/s
+    base = 0.3 * 100 * 1000
+    coef = np.sqrt(tmp / 10)
+    return base * coef
