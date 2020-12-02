842eeea9b753b7e52f5ae45563f94f063e02beb9
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-12-03 01:33:20 +0900

    add : plot

--- plot.py
+++ plot.py
@@ -1,20 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import json
+import os
 
 
 def main():
-    r_h = np.load("data/r_h.npy")
-    rho = np.load("data/rho.npy")
-    t = np.load("data/t.npy")
+    with open("configs.json", "r") as f:
+        json_open = json.load(f)
 
-    if True:
-        f = lambda x: np.log(x)
-    else:
-        f = lambda x: x
-    for i in [100, 2000, 3000, 5000, 7000, 10000, 11000]:
-        plt.plot(f(r_h[i]), f(rho[i]), label="{}".format(t[i]))
-        plt.legend()
-        plt.savefig("results/noQ{:.8}.png".format(t[i]))
+    r_h = np.load(
+        "data/" + json_open["plot_tag"] + "/step_{}_r_h.npy".format(json_open["step"])
+    )
+    rho = np.load(
+        "data/" + json_open["plot_tag"] + "/step_{}_rho.npy".format(json_open["step"])
+    )
+    t = np.load(
+        "data/" + json_open["plot_tag"] + "/step_{}_t.npy".format(json_open["step"])
+    )
+    idx = int(json_open["step"])
+    for i in np.linspace(0, idx, 5):
+        plt.plot(
+            np.log10(r_h[int(i)]),
+            np.log10((rho[int(i)])),
+            label="{} * 10^13s".format(t[int(i)] / 10 ** 13),
+        )
+    plt.xlabel("log10r cgs")
+    plt.ylabel("log10rho cgs")
+    plt.legend()
+    os.makedirs("results/" + json_open["plot_tag"], exist_ok=True)
+    plt.savefig("results/" + json_open["plot_tag"] + "/out.png")
 
 
 if __name__ == "__main__":
