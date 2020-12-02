5901dec422aea8f82be700ba597223cf37108ea9
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-12-03 01:44:18 +0900

    update : use configs

--- conditions.py
+++ conditions.py
@@ -1,14 +1,16 @@
+from file_operator import read_json
+
+json_open = read_json()
 TMP_init = 10  # or 5 or 30
 AU = 1.4598 * 10 ** 13  # cm
-GRID = 200
+GRID = json_open["GRID"]
 T_ORDER = 10 ** 13
-DT = 0.001  # * 10^13 s
-T_END = 1.2 * T_ORDER
+T_END = json_open["T_END"] * T_ORDER
 M_cc = 10 ** 33  # cloud core
 G = 6.67259 * 10 ** (-8)
-R_cc = 1000 * AU  # AU 1000 to 30000
+R_cc = json_open["radius"] * AU  # AU 1000 to 30000
 R = 8 * 10 ** 7
 
 AVG = 2.4
 KQ = 2
-CFL_CONST = 0.3
+CFL_CONST = json_open["CFL"]
