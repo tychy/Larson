295fc60f7d03734c87a2aef44f9cc21c701e72ec
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-12-06 16:08:41 +0900

    add : plotを改良

--- plot.py
+++ plot.py
@@ -5,27 +5,44 @@ import os
 import seaborn as sns
 
 from file_operator import read_json
+from conditions import GRID
 
 
 def main():
     sns.set_theme()
     config = read_json()
     r_h = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_r_h.npy".format(config["plot_step"])
+        "data/" + config["plot_tag"] + "/step_{}_r_h.npy".format(config["plot_step"]),
+        allow_pickle=True,
     )
+    r = np.load(
+        "data/" + config["plot_tag"] + "/step_{}_r.npy".format(config["plot_step"]),
+        allow_pickle=True,
+    )
+
     rho = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_rho.npy".format(config["plot_step"])
+        "data/" + config["plot_tag"] + "/step_{}_rho.npy".format(config["plot_step"]),
+        allow_pickle=True,
     )
     t = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_t.npy".format(config["plot_step"])
+        "data/" + config["plot_tag"] + "/step_{}_t.npy".format(config["plot_step"]),
+        allow_pickle=True,
     )
     idx = int(config["plot_step"])
-    for i in np.linspace(0, idx, 5):
-        plt.plot(
-            np.log10(r_h[int(i)]),
-            np.log10((rho[int(i)])),
-            label="{} * 10^13s".format(t[int(i)] / 10 ** 13),
-        )
+    cur_rho = np.max(np.floor(np.log10(rho[0])))
+    print(cur_rho)
+    i = 10
+    while i < idx:
+        if np.max(np.log10(rho[i])) >= cur_rho + 1:
+            print(r_h[i].shape)
+            print(rho[i].shape)
+            plt.plot(
+                np.log10(r_h[i][: GRID - 1]),
+                np.log10((rho[i][: GRID - 1])),
+                label="{:.5f} * 10^13s".format(t[int(i)] / 10 ** 13),
+            )
+            cur_rho = np.max(np.floor(np.log10(rho[i])))
+        i += 1
     plt.xlabel("log10r cgs")
     plt.ylabel("log10rho cgs")
     plt.legend()
