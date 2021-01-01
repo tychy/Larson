e103162c05dfdbb986629de1bf8db476b7be118c
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-01-01 19:29:00 +0900

    fix : plot_coreのインデックスを修正

--- plot_core.py
+++ plot_core.py
@@ -4,36 +4,37 @@ import json
 import os
 import seaborn as sns
 
-from file_operator import read_json
+from file_operator import read_json, read_index
 from conditions import GRID
 
 
 def main():
     sns.set_theme()
     config = read_json()
+    idx = read_index("data/" + config["plot_tag"])
+
     r_h = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_r_h.npy".format(config["plot_step"]),
+        "data/" + config["plot_tag"] + "/step_{}_r_h.npy".format(idx),
         allow_pickle=True,
     )
     r = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_r.npy".format(config["plot_step"]),
+        "data/" + config["plot_tag"] + "/step_{}_r.npy".format(idx),
         allow_pickle=True,
     )
 
     rho = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_rho.npy".format(config["plot_step"]),
+        "data/" + config["plot_tag"] + "/step_{}_rho.npy".format(idx),
         allow_pickle=True,
     )
     t = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_t.npy".format(config["plot_step"]),
+        "data/" + config["plot_tag"] + "/step_{}_t.npy".format(idx),
         allow_pickle=True,
     )
 
     tmp = np.load(
-        "data/" + config["plot_tag"] + "/step_{}_tmp.npy".format(config["plot_step"]),
+        "data/" + config["plot_tag"] + "/step_{}_tmp.npy".format(idx),
         allow_pickle=True,
     )
-    idx = read_index("data" + config["plot_tag"] + "/index.txt")
 
     i = 2
     rho_ls = []
