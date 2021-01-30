fc0067329ffa44abcc3947854d655af3ab33edbd
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2021-01-30 17:50:31 +0900

    add : 進捗管理

---
+++ docs/plot_pages.py
@@ -0,0 +1,23 @@
+import datetime
+import matplotlib.pyplot as plt
+
+# make up some data
+x = []
+y = []
+with open("pages.txt" , "r") as f:
+    line = f.readline()
+    while line:
+        line = line.strip()
+        tstr = line[0:19]
+        print(line[26:])
+        y.append(int(line[26:]))
+        tdatetime = datetime.datetime.strptime(tstr, '%Y-%m-%d %H:%M:%S')
+        x.append(tdatetime)
+        line = f.readline()
+
+# plot
+plt.plot(x,y)
+# beautify the x-labels
+plt.gcf().autofmt_xdate()
+
+plt.savefig("pages.png")
